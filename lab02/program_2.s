847f433cb980584deefcb60f74738258dbe518fb
Matteo Corain <m96.corain@gmail.com>
2018-10-18 18:16:23 +0200

    Lab #2: first version

---
+++ lab02/program_2.s
@@ -0,0 +1,58 @@
+; program_2.s
+; Performing some computations on FP values
+
+; Used registers:
+; - r1: target value for the loop
+; - r2: loop accumulator
+; - f1, f2, f3, f4: temporary storage for operands from v1, v2, v3, v4
+; - f5, f6, f7: temporary storage for the results of mul, div and add
+
+; Algorithm:
+; - Load the registers f1, f2, f3, f4 with values from v1, v2, v3, v4
+; - Perform the required operations saving the results in f5, f6, f7
+; - Store the values of f5, f6, f7 into v5, v6, v7
+; - Loop for 18 times (vectors have this dimension because of the memory limitation of 1 kB)
+
+; Optimizations:
+
+        .data
+
+v1:     .double 81.4724, 15.7613, 65.5741, 70.6046, 43.8744, 27.6025, 75.1267, 84.0717, 35.1660,  7.5854
+        .double 90.5792, 97.0593,  3.5712,  3.1833, 38.1558, 67.9703, 25.5095, 25.4282
+
+v2:     .double 16.2182, 45.0542, 10.6653, 43.1414, 85.3031, 41.7267, 78.0252, 23.4780, 54.7009, 92.9386
+        .double 79.4285,  8.3821, 96.1898, 91.0648, 62.2055, 4.9654 , 38.9739, 35.3159
+
+v3:     .double 64.4318, 20.7742, 31.1102, 59.4896,  8.5516, 96.3089,  3.7739, 10.6762,  3.0541, 18.2922
+        .double 37.8609, 30.1246, 92.3380, 26.2212, 26.2482, 54.6806, 88.5168, 65.3757
+
+v4:     .double  5.9619, 65.9605, 17.3389, 29.1984, 95.1630, 30.1455,  3.2601, 64.4765, 25.1806, 90.6308
+        .double 68.1972, 51.8595, 39.0938, 43.1651, 92.0332, 70.1099, 56.1200, 37.6272
+
+v5:     .space 144
+v6:     .space 144
+v7:     .space 144
+
+        .text
+
+main:   daddui  r1, r0, 144             ; load r1
+        daddui  r2, r0, 0               ; load r2
+
+loop:   l.d     f1, v1(r2)              ; load f1
+        l.d     f2, v2(r2)              ; load f2
+        l.d     f3, v3(r2)              ; load f3
+        l.d     f4, v4(r2)              ; load f4
+        
+        daddui  r2, r2, 8               ; increment r2
+
+        mul.d   f5, f1, f2              ; f5 = f1 * f2
+        div.d   f6, f2, f3              ; f6 = f2 * f3
+        add.d   f7, f1, f4              ; f7 = f1 * f4
+
+        s.d     f5, v5(r2)              ; store f5
+        s.d     f6, v6(r2)              ; store f6
+        s.d     f7, v7(r2)              ; store f7
+
+        bne     r1, r2, loop            ; loop if r1 != r2
+
+        halt
---
+++ lab02/program_3.s
@@ -0,0 +1,67 @@
+; program_2.s
+; Parity bit computation
+
+; Used registers:
+; - r1: accumulator for loop1
+; - r2: accumulator for loop2
+; - r3: current byte
+; - r4: working byte
+; - r5: current parity bit
+; - r6: parity bit sum
+
+; Algorithm:
+; - The byte is stored into r3 and r4
+; - An and operation with 00000001 and r4 is computed and stored in r5, to isolate the LSB
+; - The result is put in xor with the calculated parity bit in r6, so that if the LSB is 1 it changes its value
+; - The working byte is shifted right arithmetically by one position
+; - The operation is repeated 7 times (loop2)
+; - If the calculated parity bit is 1 (odd), an or is performed between the original byte and 10000000, to set the MSB to 1
+; - If the calculated parity bit is 0 (even), an and is performed between the original byte and 01111111, to set the MSB to 0
+; - The operation is repeated for every element in the vector (loop1)
+
+; Optimizations:
+
+        .data
+
+vector: .byte   0x1c, 0x1b, 0x78, 0x76, 0x15, 0x33, 0x0f, 0x40
+        .byte   0x64, 0x72, 0x0a, 0x0f, 0x4a, 0x79, 0x6a, 0x04
+        .byte   0x3c, 0x22, 0x6b, 0x48, 0x73, 0x2f, 0x7b, 0x09
+        .byte   0x75, 0x3a, 0x29, 0x4b, 0x12, 0x3a, 0x50, 0x4d
+        .byte   0x39, 0x53, 0x2e, 0x60, 0x7e, 0x4d, 0x68, 0x69
+        .byte   0x65, 0x3d, 0x4f, 0x2c, 0x02, 0x1f, 0x63, 0x4a
+        .byte   0x62, 0x01, 0x7f, 0x69, 0x0b, 0x3e, 0x23, 0x24
+        .byte   0x66, 0x6c, 0x5f, 0x7f, 0x2c, 0x06, 0x67, 0x67
+        .byte   0x0c, 0x1b, 0x59, 0x20, 0x2c, 0x39, 0x39, 0x1d
+        .byte   0x24, 0x0c, 0x58, 0x32, 0x05, 0x3d, 0x6d, 0x43
+        .byte   0x6c, 0x3f, 0x32, 0x21, 0x7e, 0x44, 0x24, 0x6d
+        .byte   0x32, 0x38, 0x5b, 0x34, 0x61, 0x48, 0x2a, 0x09
+        .byte   0x6e, 0x66, 0x63, 0xe0
+
+        .text
+
+main:   daddi  r1, r0, 100              ; r1 = 100
+
+loop1:  daddi   r1, r1, -1              ; decrement r1
+        lbu     r3, vector(r1)          ; load value
+        
+        daddi   r2, r0, 7               ; r2 = 7
+        daddi   r4, r3, 0               ; r4 = r3
+        daddi   r6, r0, 0               ; r6 = 0
+
+loop2:  andi    r5, r4, 0x01            ; r5 = r4 and 00000001
+        xor     r6, r6, r5              ; r6 = r6 xor r5
+        dsrl    r4, r4, 1               ; shift by 1
+        daddi   r2, r2, -1              ; decrement r2
+        bnez    r2, loop2               ; loop 2
+
+        beqz    r6, even                ; branch for even parity
+
+odd:    ori     r3, r3, 0x80            ; set MSB to 1
+        j       next                    ; skip even case
+
+even:   andi    r3, r3, 0x7f            ; set MSB to 0
+
+next:   sb      r3, vector(r1)          ; store value
+        bnez    r1, loop1               ; loop 1
+
+        halt
