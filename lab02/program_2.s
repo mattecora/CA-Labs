66529b4ea2de8e59003606e884b4b488039f335e
Matteo Corain <m96.corain@gmail.com>
2018-10-18 19:48:54 +0200

    Lab #2: optimizations on program_2

--- lab02/program_2.s
+++ lab02/program_2.s
@@ -14,6 +14,10 @@
 ; - Loop for 18 times (vectors have this dimension because of the memory limitation of 1 kB)
 
 ; Optimizations:
+; - Rearranging of the load instructions before the respective operations
+; - Reordering of the div before the mul to reduce total clock cycles
+; - Moving of the r2 update between the operations and the store instructions
+; - Rearranging of the store instructions to store before the data available before
 
         .data
 
@@ -21,7 +25,7 @@ v1:     .double 81.4724, 15.7613, 65.5741, 70.6046, 43.8744, 27.6025, 75.1267, 8
         .double 90.5792, 97.0593,  3.5712,  3.1833, 38.1558, 67.9703, 25.5095, 25.4282
 
 v2:     .double 16.2182, 45.0542, 10.6653, 43.1414, 85.3031, 41.7267, 78.0252, 23.4780, 54.7009, 92.9386
-        .double 79.4285,  8.3821, 96.1898, 91.0648, 62.2055, 4.9654 , 38.9739, 35.3159
+        .double 79.4285,  8.3821, 96.1898, 91.0648, 62.2055,  4.9654, 38.9739, 35.3159
 
 v3:     .double 64.4318, 20.7742, 31.1102, 59.4896,  8.5516, 96.3089,  3.7739, 10.6762,  3.0541, 18.2922
         .double 37.8609, 30.1246, 92.3380, 26.2212, 26.2482, 54.6806, 88.5168, 65.3757
@@ -38,20 +42,21 @@ v7:     .space 144
 main:   daddui  r1, r0, 144             ; load r1
         daddui  r2, r0, 0               ; load r2
 
-loop:   l.d     f1, v1(r2)              ; load f1
-        l.d     f2, v2(r2)              ; load f2
+loop:   l.d     f2, v2(r2)              ; load f2
         l.d     f3, v3(r2)              ; load f3
-        l.d     f4, v4(r2)              ; load f4
+        div.d   f6, f2, f3              ; f6 = f2 / f3
         
+        l.d     f1, v1(r2)              ; load f1
+        mul.d   f5, f1, f2              ; f5 = f1 * f2
+
+        l.d     f4, v4(r2)              ; load f4
+        add.d   f7, f1, f4              ; f7 = f1 + f4
+
         daddui  r2, r2, 8               ; increment r2
 
-        mul.d   f5, f1, f2              ; f5 = f1 * f2
-        div.d   f6, f2, f3              ; f6 = f2 * f3
-        add.d   f7, f1, f4              ; f7 = f1 * f4
-
+        s.d     f7, v7(r2)              ; store f7
         s.d     f5, v5(r2)              ; store f5
         s.d     f6, v6(r2)              ; store f6
-        s.d     f7, v7(r2)              ; store f7
 
         bne     r1, r2, loop            ; loop if r1 != r2
 
